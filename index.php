ba7b15e5a66713047a669647317f4bc9a930fa21
Caramiozz <56002327+Caramiozz@users.noreply.github.com>
2022-01-05 12:59:41 +0300

    Update index.php

--- index.php
+++ index.php
@@ -100,7 +100,7 @@
       
       <div id="headerLeft">
           
-         <img src="public/images/Resim2.png" id='pageTitle'/>
+         <img src="https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&w=1000&q=80" id='pageTitle'/>
 
       </div>
       
